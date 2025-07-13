72824effcbef330f2e28187e55e91909ecbbef97
natkhatishigh <iamshivanshsood@gmail.com>
2025-07-13 17:32:54 +0530

    feat: integrated lombok

--- journalApp/src/main/java/com/project/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/com/project/journalApp/entity/JournalEntry.java
@@ -1,6 +1,9 @@
 package com.project.journalApp.entity;
 
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +12,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries") //This annotation tells that this entity is mapped with mongodb collection
+@Data
 public class JournalEntry {
 
     @Id     //Datatype for mongodb id
@@ -19,39 +23,5 @@ public class JournalEntry {
     private String content;
 
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
 
 }
