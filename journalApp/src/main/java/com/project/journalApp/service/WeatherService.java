037e3c9619914ae28b1ecd3bcb16c626e78c54e5
natkhatishigh <iamshivanshsood@gmail.com>
2025-07-30 16:55:40 +0530

    fix: changed @Component annotation to @Service in service package.

--- journalApp/src/main/java/com/project/journalApp/service/WeatherService.java
+++ journalApp/src/main/java/com/project/journalApp/service/WeatherService.java
@@ -5,9 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 
 public class WeatherService {
 
