263e762950972c99e0908895f5146e38a3faf11f
natkhatishigh <iamshivanshsood@gmail.com>
2025-07-30 17:23:52 +0530

    Accidently exposed my api access key, fixed it now

--- journalApp/src/main/java/com/project/journalApp/service/WeatherService.java
+++ journalApp/src/main/java/com/project/journalApp/service/WeatherService.java
@@ -2,17 +2,17 @@ package com.project.journalApp.service;
 
 import com.project.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-
 public class WeatherService {
 
-    private static final String apiKey = "e882da72bdcf49252ee2c5afff3e8c82";
+    @Value("${weather.api.key}")
+    private String apiKey;
 
     private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
