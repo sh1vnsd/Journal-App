36872a3d316bf4132431c41b6f10f9a360ca967e
natkhatishigh <iamshivanshsood@gmail.com>
2025-08-21 15:36:32 +0530

    feat: Used JavaMailSender to Send mails also removed sensitive spring boot config files.

---
+++ journalApp/src/test/java/com/project/journalApp/service/EmailServiceTests.java
@@ -0,0 +1,17 @@
+package com.project.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendEmail() {
+        emailService.sendEmail("shivanshsood555@gmail.com", "Testing java mail sender", "hi");
+    }
+}
